92bb989949391d2f66d355cfbfec29d34854c135
tvlmedia <info@tvlmedia.nl>
2026-01-09 19:16:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -211,13 +211,6 @@ const notes = {
 };
 
 const lensImageMap = {
-  "ironglass_red_p_35mm_t2_8":"red_p_37mm_t2_8.jpg",
-  "ironglass_red_p_50mm_t2_8":"red_p_58mm_t2_8.jpg",
-  "ironglass_red_p_75mm_t2_8":"red_p_85mm_t2_8.jpg",
-  "ironglass_mkii_35mm_t2_8":"mkii_37mm_t2_8.jpg",
-  "ironglass_mkii_50mm_t2_8":"mkii_50mm_t2_8.jpg",
-  "ironglass_mkii_75mm_t2_8":"mkii_85mm_t2_8.jpg",
-  "ironglass_zeiss_jena_75mm_t2_8":"jena_80mm_t2_8.jpg",
 };
 
 const lensDescriptions = {
