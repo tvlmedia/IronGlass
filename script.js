591eb27e43fef158d494e0777b455f80fb690bab
tvlmedia <info@tvlmedia.nl>
2026-01-09 18:00:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -211,6 +211,7 @@ const notes = {
   "dzo_vespid_80mm":"75mm","dzo_vespid_85mm":"75mm","dzo_arles_80mm":"75mm","dzo_arles_85mm":"75mm","lomo_standard_speed_80mm":"75mm","lomo_standard_speed_85mm":"75mm",
   "cooke_panchro_ff_80mm":"75mm","cooke_panchro_ff_85mm":"75mm","ironglass_zeiss_jena_80mm":"75mm","ironglass_zeiss_jena_85mm":"75mm","ironglass_red_p_80mm":"75mm",
   "ironglass_red_p_85mm":"75mm","ironglass_mkii_80mm":"75mm","ironglass_mkii_85mm":"75mm","ironglass_red_p_75mm":"85mm","ironglass_mkii_75mm":"85mm","ironglass_zeiss_jena_75mm":"80mm",
+  "ironglass_sovjet_mkii_120mm": "135mm",
 };
 
 const lensImageMap = {
